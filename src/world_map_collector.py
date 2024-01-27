d4596e2a0c73d0f1d336b1cbfab4598db8b96b33
Tony Tian <t.tian4321@gmail.com>
2024-01-27 11:20:23 -0600

    black formatter

--- src/world_map_collector.py
+++ src/world_map_collector.py
@@ -12,8 +12,10 @@ for index, world_map_id in enumerate(all_world_maps):
     if index % 20 == 0:
         print(f"Processing {index}/{size_all_world_maps}...")
 
-    world_map_details_endpoint = f"{world_map_conf.BASE_URL_ENDPOINT_BUILDER}/{world_map_id}"
-    
+    world_map_details_endpoint = (
+        f"{world_map_conf.BASE_URL_ENDPOINT_BUILDER}/{world_map_id}"
+    )
+
     world_map_details: dict = session.get(world_map_details_endpoint).json()
 
     parent_world_map_name = world_map_details.get("parentWorld")
@@ -38,23 +40,23 @@ for index, world_map_id in enumerate(all_world_maps):
         with open(target_file, "wb") as fw:
             fw.write(decoded_image_data)
 
-        search_index_payload = {
-            "target_file_name_builder": target_file_name_builder
-        }
+        search_index_payload = {"target_file_name_builder": target_file_name_builder}
 
         # NOTE: regional world maps contain 'links' to area maps - index this for faster reads on the web app
-        is_region_map = (len(world_map_details.get("links", [])) > 0)
+        is_region_map = len(world_map_details.get("links", [])) > 0
 
         mongo_metadata_payload_builder = {
             "target_file_name_builder": target_file_name_builder,
             "is_region_map": is_region_map,
-            "raw": world_map_details
+            "raw": world_map_details,
         }
 
         """
         NOTE push coordinate data for each map's 'dot' to MongoDB
         """
-        generic_push_metadata(WORLD_MAPS_COLL, search_index_payload, mongo_metadata_payload_builder)
+        generic_push_metadata(
+            WORLD_MAPS_COLL, search_index_payload, mongo_metadata_payload_builder
+        )
     else:
         print(f"Missing image data - skipping {target_world_map}...")
         continue
